c89bc1614a51dd4ec8a418df1ae2f50a79a9515e
DaniilChern <46822766+DaniilChern@users.noreply.github.com>
2019-02-07 12:07:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let inpLog = prompt ('Введите логин ','');
+let inpPass = prompt ('Введите пароль', '');
+if (inpLog == 'Ivan'&& inpPass == 3|| inpLog =='Alex'&& inpPass == 4|| inpLog == 'Petr'&& inpPass==5){
+   alert (' hello');}
+   else {
+      alert('Вы ввели неправильный логин или пароль');
+   }
